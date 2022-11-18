6626593f70f249d781b2a0024403391164391e75
valeria pallas <valerianpallas@gmail.com>
2022-11-18 12:11:31 +0100

    add dotenv-rails

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, presence: true, length: {minimum: 6}
-  validates :movie, uniqueness: {scope: :list}
-  validates :list, uniqueness: {scope: :movie}
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list }
+  validates :list, uniqueness: { scope: :movie }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class List < ApplicationRecord
-  validates :name, presence: true, :uniqueness => true
+  validates :name, presence: true, uniqueness: true
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
 end
